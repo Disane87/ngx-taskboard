8d6a2cb871dff818d7b53416a8f97f909c170dd0
Marco <Disane87@users.noreply.github.com>
2019-09-01 23:19:58 +0200

    Dev (#53)
    * ci: travis build
    
    * ci: added travis npm cache
    
    * test(codecov): Addes spec
    
    * ci: added chrome to travis
    
    * test: Addes test files for board, added fa testbed
    
    * perf(travis & npm): using npm ci instead of npm install
    
    * ci: added codecov
    
    * ci: Codecov
    
    * ci: changed travis

---
+++ projects/ngx-taskboard-lib/src/lib/board/board.component.spec.ts
@@ -0,0 +1,39 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BoardComponent } from './board.component';
+import { FilterSearchBarComponent } from '../filter-search-bar/filter-search-bar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule } from '@angular/forms';
+import { TaskboardService } from '../taskboard.service';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas, far, fab);
+
+
+describe('MyLibComponent', () => {
+  let component: BoardComponent;
+  let fixture: ComponentFixture<BoardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BoardComponent, FilterSearchBarComponent ],
+      imports: [ FontAwesomeModule, FormsModule ],
+      providers: [ TaskboardService ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BoardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
