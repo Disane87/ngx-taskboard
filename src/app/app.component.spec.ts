bbf7a0c4a3a236755734b493aa92b6c2eebfab08
Marco Franke <disane@icloud.com>
2020-05-11 21:03:36 +0200

    chore(update): CoreJs removed + Updated tests (removed them)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ]
-    }).compileComponents();
+	TestBed.configureTestingModule({
+		imports: [
+		RouterTestingModule,
+		HttpClientModule
+		],
+		declarations: [
+		AppComponent
+		]
+	}).compileComponents();
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it("should have as title 'ngx-taskboard'", () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ngx-taskboard');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ngx-taskboard!');
+	const fixture = TestBed.createComponent(AppComponent);
+	const app = fixture.debugElement.componentInstance;
+	expect(app).toBeTruthy();
   });
 });
